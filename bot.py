8a4c7f0bd0bd9007ba91a842a7a0050b4736cbd6
Ananya Nayak <ananyanayak102@gmail.com>
2023-07-28 22:46:42 +0530

    feat: add fun commands

--- bot.py
+++ bot.py
@@ -1,24 +1,65 @@
 import os
-import logging
+import re
+import random
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
-from dotenv import load_dotenv
 from pathlib import Path
+from sympy import sympify, SympifyError
+from dotenv import load_dotenv
+import requests
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-logger = logging.getLogger(__name__)
 
 # Initializes your app with your bot token and socket mode handler
 app = App(token=os.environ["SLACK_BOT_TOKEN"], name="Nerdbot")
 
 
-# Listens to incoming messages that contain "hello"
-@app.message("hello")
-def message_hello(message, say):
-    # say() sends a message to the channel where the event was triggered
-    say(text=f"Hey there <@{message['user']}>!")
-    logger.info(f"Message sent to {message['channel']}")
+@app.command("/hello")
+def handle_hello(say, command):
+    say(f"Hey <@{command['user_id']}>!")
+
+
+@app.event("member_joined_channel")
+def welcome_message(event, say):
+    say(f"Hey <@{event['user']}>! Welcome to {event['channel']}. :tada:")
+
+
+@app.command("/nasa-pic")
+def handle_nasa_pic(say):
+    response = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={os.environ['NASA_API_KEY']}")
+    blocks = [
+        {
+            "type": "image",
+            "title": {
+                "type": "plain_text",
+                "text": response.json()["title"]
+            },
+            "image_url": response.json()["url"],
+            "alt_text": response.json()["title"]
+        }
+    ]
+    say(blocks=blocks)
+
+
+@app.command("/suggest-activity")
+def handle_activity(say, command):
+    activity = requests.get("https://www.boredapi.com/api/activity").json()
+    say(f"Hey <@{command['user_id']}>, how about *{activity['activity']}*?")
+
+
+@app.message(re.compile(r"^([-+*/^%!().\d\s]+)$"))
+def calc(message, context, say):
+    try:
+        formula = context["matches"][0]
+        result = sympify(formula)
+        if result.is_Integer:
+            answer = int(result)
+        else:
+            answer = float(result)
+        say(f"Hey <@{message['user']}>, the answer is *{answer}*")
+    except SympifyError:
+        say(f"Hey <@{message['user']}>, I can't solve that :(")
 
 
 # Start your app
